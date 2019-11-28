12556e956b522d1c92130d776cf28f867540f263
Katarina Uvanovic <katarina.uvanovic@outlook.com>
2019-11-28 23:53:14 +0000

    .

--- javascript.js
+++ javascript.js
@@ -1,6 +1,12 @@
 function Rating(starRating) {
 
-    
+    for (var i = 1; i < 6; i++){
+        console.log("rating" + i)
+        var element = document.getElementById("rating" + i);
+        if (element.classList.contains("checked")){
+            element.classList.remove("checked");
+        }
+    }
 
     var i;
     for (i = 1; i <= starRating; i+=1) {
